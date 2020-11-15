5b9f2f6de5aa825050e8d13bcb3bb4d54f9dfe7e
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2020-11-15 13:33:05 +0500

    simple timer app without css

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//importing timer component
+import Timer from '../Timer/Timer';
+import '../Timer/Timer'
+import './App.css';
+
+const App = () => (
+    <div className='container'>
+        <Timer />
+    </div>
+)
+export default App;
